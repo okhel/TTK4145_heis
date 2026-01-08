2a6963fd8c2074390bb983de8c5ba9568b4399bd
Ole <olekhel@stud.ntnu.no>
2026-01-08 08:07:29 +0100

    solve exercise 1 using rust

--- Exercise1/9 - other languages/Rust/src/main.rs
+++ Exercise1/9 - other languages/Rust/src/main.rs
@@ -7,7 +7,7 @@ use std::sync::{Arc, Mutex};
 fn main() {
     // TODO: Find out what Arc is and why it is needed?
     // TODO: You need to add a Mutex, should it be Arc<Mutex<i32>> or Mutex<Arc<i32>>?
-    let i: Arc<i32> = Arc::new(0);
+    let i: Arc<Mutex<i32>> = Arc::new(Mutex::new(0));
 
     let i_incrementing = i.clone();
     let i_decrementing = i.clone();
@@ -15,24 +15,25 @@ fn main() {
     let join_incrementing = thread::spawn(move || {
         for _ in 0..1_000_000 {
             // TODO: aquire the lock before using i
-            *i_incrementing += 1;
+            *i_incrementing.lock().unwrap() += 1;
             // Do you have to release the mutex here?
         }
+        i_incrementing
     });
-    
+
     let join_decrementing = thread::spawn(move || {
         for _ in 0..1_000_000 {
             // TODO: aquire the lock before using i
-            *i_decrementing -= 1;
+            *i_decrementing.lock().unwrap() -= 1;
             // Do you have to release the mutex here?
         }
 
     });
 
-    join_incrementing.join().unwrap();
+    let v_back = join_incrementing.join().unwrap();
     join_decrementing.join().unwrap();
 
     // TODO: aquire the lock before using i
-    println!("The number is: {}", *i);
+    println!("The number is: {}", *v_back.lock().unwrap());
 }
 
