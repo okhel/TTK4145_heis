5697a418441dfa988090965930d3a92a8ba49467
Hans Christian <haavagr@ntnu.no>
2026-01-15 12:04:21 +0100

    add state enum

--- Exercise3/single_elevator/src/elevator.rs
+++ Exercise3/single_elevator/src/elevator.rs
@@ -6,18 +6,24 @@ use elevio::elev::Elevio;
 use std::io::*;
 
 
-//pub use elevator_control::{move_to_floor};
-
 const NUM_FLOORS: u8 = 4;
+
+enum EStates {
+    DirUp,
+    DirDown,
+    Stationary,
+}
 
 pub struct Elevator {
     elev_io: Elevio,
+    state: EStates,
 }
 
 impl Elevator {
     pub fn init() -> Result<Elevator> {
         Ok(Self {
-            elev_io: Elevio::init("localhost:15657", NUM_FLOORS)?
+            elev_io: Elevio::init("localhost:15657", NUM_FLOORS)?,
+            state: EStates::Stationary,
         })
     }
 }
