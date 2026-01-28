d99a4123f204a30dbf9405989b3ddd34f6a1d369
Karoline Garsjø <karolsga@stud.ntnu.no>
2026-01-28 13:10:37 +0100

    first attempt at UDP communication

--- share_code.sh
+++ share_code.sh
@@ -2,3 +2,8 @@ sshpass -f passwordfile ssh student@10.100.23.$1 "rm -rf sanntid10"
 echo "Deleted old project files"
 sshpass -f passwordfile scp -r ~/sanntid10 student@10.100.23.$1:sanntid10
 echo "Transferred new project files"
+
+sshpass -f passwordfile ssh student@10.100.23.$2 "rm -rf sanntid10"
+echo "Deleted old project files"
+sshpass -f passwordfile scp -r ~/sanntid10 student@10.100.23.$2:sanntid10
+echo "Transferred new project files"
