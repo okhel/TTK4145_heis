d5f4df11c80ce4b7eb546492a96f015156b482f2
Ole Helgedagsrud <olekaasa@gmail.com>
2026-02-05 08:32:11 +0100

    refactor goto_floor to use channels

--- Exercise3/single_elevator/src/main.rs
+++ Exercise3/single_elevator/src/main.rs
@@ -9,21 +9,15 @@ pub mod networking;
 
 async fn main() -> io::Result<()> {
 
-    let id: u8 = std::env::args().last().unwrap().parse().unwrap();
-    let mut ids = vec![19, 20, 21];
+    // let id: u8 = std::env::args().last().unwrap().parse().unwrap();
+    // let mut ids = vec![19, 20, 21];
 
-    ids.retain(|x| *x !=id);
-    println!("Connecting to {}", ids[1]);
+    // ids.retain(|x| *x !=id);
+    // println!("Connecting to {}", ids[1]);
 
-    networking::udptest(ids[1]).await;
-
-    let my_elev = elevator::Elevator::init().await?; // give ownership of channels to object    
+    // networking::udptest(ids[1]).await;
     
-    for _ in 0..100  {
-        my_elev.goto_floor(1).await;
-        my_elev.goto_floor(4).await;
-        my_elev.goto_floor(2).await;
-        my_elev.goto_floor(3).await;
-    }
+    elevator::elevator_runner().await?;
+    
     Ok(())
 }
